c68625d871a02916de61836ad1a2dd8a77b19395
Franco Wagenhöfer <franwagen.500@gmail.com>
2025-12-20 00:57:41 -0300

    Configuración paquetes NuGet + DbContext

---
+++ Data/MovieDbContext.cs
@@ -0,0 +1,22 @@
+﻿using app_movie_mvc.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace app_movie_mvc.Data
+{
+    public class MovieDbContext : IdentityDbContext<Usuario>
+    // IdentityDbContext con la clase Usuario personalizada para la autenticación y autorización
+    // Hereda de IdentityDbContext para incluir las tablas de identidad en la base de datos
+    // IdentityDbContext<Usuario> permite usar la clase Usuario personalizada en lugar de la clase IdentityUser predeterminada
+    // Esto es útil para agregar propiedades adicionales al usuario, como Nombre, Apellido, etc.
+    {
+        public MovieDbContext(DbContextOptions<MovieDbContext> options) : base(options) { }
+
+        public DbSet<Pelicula> Peliculas { get; set; }
+        public DbSet<Genero> Generos { get; set; }
+        public DbSet<Plataforma> Plataformas { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Favorito> Favoritos { get; set; }
+
+    }
+}
--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -1,6 +1,8 @@
-﻿namespace app_movie_mvc.Models
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace app_movie_mvc.Models
 {
-    public class Usuario
+    public class Usuario: IdentityUser // Sirve para extender la clase IdentityUser y agregar propiedades personalizadas
     {
         public string Nombre { get; set; }
         public string Apellido { get; set; }
@@ -9,8 +11,7 @@
         public string ImagenUrlPerfil { get; set; }
 
         public List<Favorito>? PeliculasFavoritas { get; set; } // Lista de peliculas favoritas del usuario
+        public List<Review>? ReviewsUsuario { get; set; } // Lista de reviews hechas por el usuario
 
-
-        //sas
     }
 }
