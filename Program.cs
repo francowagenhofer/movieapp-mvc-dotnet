8e2a6a94eb6da5c7d2452f9088b765409c62d2d2
Franco Wagenhöfer <franwagen.500@gmail.com>
2025-12-20 02:54:32 -0300

    DatatableKS + Dark Mode

--- Program.cs
+++ Program.cs
@@ -10,8 +10,25 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<MovieDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("MovieDbContext")));
 
-
 var app = builder.Build();
+
+// Invocacion de la ejecucion de la siembra de datos en la base de datos
+using (var scope = app.Services.CreateScope())
+{
+    try
+    {
+        var services = scope.ServiceProvider;
+        var context = services.GetRequiredService<MovieDbContext>();
+        DbSeeder.Seed(context);
+    }
+    catch (Exception ex)
+    {
+        var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred seeding the DB.");
+    }
+
+}
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
